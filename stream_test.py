948bdbe17f9a289ae646cbf426c8b75074655e33
KimBS <born9608@gmail.com>
2023-07-27 16:51:52 +0900

    서브탭 생성 및 통합 업뎃

--- stream_test.py
+++ stream_test.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from eda import abal_data_eda, steel_data_eda
 
+# 별 딥러닝모델
 from model.star_model.final import star_deep_model
 
 
@@ -63,14 +64,16 @@ if choose == "전복(Abalone)":
     columns_list =  df_abal.columns.to_list()
 
     if selected_menu == "데이터 설명":
-        listTabs = [
-            "특성 설명",
-            "데이터 프레임 보기"
-            ]
-        tab1, tab2 = st.tabs(listTabs)
+        selected_sub_menu = option_menu(None, ["특성 설명", "데이터 프레임 보기", "데이터 인사이트"],
+                                        menu_icon="cast", default_index=0, orientation="horizontal", 
+                                        styles = {"container": {"padding": "0!important", "background-color": "#fafafa"},
+                                                    "nav-link": {"font-size": "15px", "text-align": "center", "margin":"10px", "--hover-color": "#FFE192"},
+                                                    "icon": {"color": "orange", "font-size": "18px"}, 
+                                                    "nav-link-selected": {"background-coloaddr": "#FFC939"},
+                                                })
 
-        with tab1:
-            st.header("전복 데이터")
+
+        if selected_sub_menu == "특성 설명":
             # 그림 두개 그리기
             abal_col1, abal_col2 = st.columns(2)
             with abal_col1:
@@ -78,6 +81,7 @@ if choose == "전복(Abalone)":
             with abal_col2:
                 st.image("abalone.jpeg", use_column_width=True)
             
+            # 특성 설명
             st.markdown('- **Sex** : 전복 성별 / object : F, M, I로 구성되며 I는 유아기-전복은 자웅동체이다가 성숙하면서 성별이 고정되는 경향이 있다')
             st.markdown('- **Length** : 전복 길이 / mm / float : 껍질의 최대 길이를 의미 상단 그림 참고')
             st.markdown('- **Diameter** : 전복 지름 / mm / float : Length를 쟀을 때와 수직으로 잴 때의 길이를 말함')
@@ -91,8 +95,12 @@ if choose == "전복(Abalone)":
             st.markdown('연속성을 가지는 특성(성별과 나이를 제외한)은 원 수치에 200을 나눈 값이라 한다')
             st.markdown('EDA를 위해 임시로 200을 곱해 확인한다')
 
-        with tab2:
+        elif selected_sub_menu == "데이터 프레임 보기":
+            st.header("전복 데이터")
             st.dataframe(df_abal)
+
+        else:
+            st.write("전복 데이터 인사이트가 들어갈 페이지입니다")
 
     if selected_menu == "데이터 시각화":
         st.title('특성 별 분포 및 시각화')
@@ -145,8 +153,8 @@ if choose == "전복(Abalone)":
                 plt.title(f'{option} Box plot')
                 st.pyplot(plt)
 
-    else:
-        pass
+    if selected_menu == "모델 예측":
+        st.write("모델 예측 성능이 들어갈 페이지입니다")
 
 
 if choose == "중성자별(Star)":
@@ -154,40 +162,59 @@ if choose == "중성자별(Star)":
                                 icons=['bi bi-file-earmark', 'kanban','bi bi-gear'],
                                 menu_icon="cast", default_index=0, orientation="horizontal")
 
-
     if selected_menu == "데이터 설명":
-        st.header("중성자별 데이터")
-        st.image("star.jpeg", use_column_width=True)
-        st.markdown('- **Mean of the Integrated Profile (통합 프로파일의 평균)**')
-        st.write('통합 프로파일에서 얻은 측정값들의 평균값을 나타냅니다.')
-        st.write('통합 프로파일에서 얻은 측정값들의 평균값을 나타냅니다.')
-        st.write('통합 프로파일은 여러 개의 라디오 펄스를 하나로 통합한 것으로, 천체의 특성을 반영하는 신호들이 포함되어 있습니다.')
-        st.write('이 평균값은 천체의 펄스 특성을 나타내는 중심적인 측정치입니다.')
-        st.markdown('- **Standard Deviation of the Integrated Profile (통합 프로파일의 표준 편차)**')
-        st.write('통합 프로파일에서 얻은 측정값들의 편차 또는 산포를 나타냅니다.')
-        st.write('평균값으로부터 얼마나 떨어져 있는지를 평가하여 데이터의 퍼짐 정도를 표현합니다.')
-        st.write('표준 편차가 크면 천체의 펄스 강도가 불규칙적으로 변하는 것을 의미할 수 있습니다.')
-        st.markdown('- **Excess Kurtosis of the Integrated Profile (통합 프로파일의 첨도)**')
-        st.write('첨도는 확률 변수의 분포에서 꼬리 부분의 상대적인 두께를 나타내는 지표로서, 데이터가 정규 분포에서 얼마나 벗어난 분포를 갖고 있는지를 나타냅니다.')
-        st.write('첨도가 0보다 크면 뾰족한 분포로, 0보다 작으면 완만한 분포로 해석할 수 있습니다.')
-        st.markdown('- **Skewness of the Integrated Profile (통합 프로파일의 비대칭도)**')
-        st.write('데이터의 비대칭 정도를 나타내는 지표로서, 평균을 기준으로 얼마나 좌우로 치우쳐져 있는지를 나타냅니다.')
-        st.write('양수면 오른쪽으로, 음수면 왼쪽으로 치우쳐진 분포를 의미합니다.')
-        st.markdown('- **Mean of the DM-SNR Curve (DM-SNR 곡선의 평균)**')
-        st.write('DM-SNR 곡선은 주기성이 있는 신호에서 찾아지는 신호 대 잡음 비율(DM-SNR) 값을 나타내는 곡선입니다.')
-        st.write('이 곡선에서 추출한 측정값들의 평균을 나타냅니다.')
-        st.markdown('- **Standard Deviation of the DM-SNR Curve (DM-SNR 곡선의 표준 편차)**')
-        st.write('DM-SNR 곡선에서 추출한 측정값들의 편차 또는 산포를 나타냅니다.')
-        st.write('평균값으로부터 얼마나 떨어져 있는지를 평가하여 데이터의 퍼짐 정도를 표현합니다.')
-        st.markdown('- **Excess Kurtosis of the DM-SNR Curve (DM-SNR 곡선의 첨도)**')
-        st.write('DM-SNR 곡선에서 추출한 측정값들의 첨도를 나타냅니다.')
-        st.write('이 값이 0보다 크면 뾰족한 분포로, 0보다 작으면 완만한 분포로 해석할 수 있습니다.')
-        st.markdown('- **Skewness of the DM-SNR Curve (DM-SNR 곡선의 비대칭도)**')
-        st.write('DM-SNR 곡선에서 추출한 측정값들의 비대칭 정도를 나타냅니다.')
-        st.write('양수면 오른쪽으로, 음수면 왼쪽으로 치우쳐진 분포를 의미합니다.')
-        st.markdown('- **traget_class**')
-        st.write('1인 경우 중성자별, 0인 경우 중성자별 X')
+        selected_sub_menu = option_menu(None, ["특성 설명", "데이터 프레임 보기", "데이터 인사이트"],
+                                        menu_icon="cast", default_index=0, orientation="horizontal", 
+                                        styles = {"container": {"padding": "0!important", "background-color": "#fafafa"},
+                                                    "nav-link": {"font-size": "15px", "text-align": "center", "margin":"10px", "--hover-color": "#FFE192"},
+                                                    "icon": {"color": "orange", "font-size": "18px"}, 
+                                                    "nav-link-selected": {"background-coloaddr": "#FFC939"},
+                                                })
+        if selected_sub_menu == "특성 설명":
+            # star.jpeg 삽입
+            st.image("star.jpeg", use_column_width=True)
+
+            # 특성 설명
+            st.header("중성자별 데이터")
+            st.image("star.jpeg", use_column_width=True)
+            st.markdown('- **Mean of the Integrated Profile (통합 프로파일의 평균)**')
+            st.write('통합 프로파일에서 얻은 측정값들의 평균값을 나타냅니다.')
+            st.write('통합 프로파일에서 얻은 측정값들의 평균값을 나타냅니다.')
+            st.write('통합 프로파일은 여러 개의 라디오 펄스를 하나로 통합한 것으로, 천체의 특성을 반영하는 신호들이 포함되어 있습니다.')
+            st.write('이 평균값은 천체의 펄스 특성을 나타내는 중심적인 측정치입니다.')
+            st.markdown('- **Standard Deviation of the Integrated Profile (통합 프로파일의 표준 편차)**')
+            st.write('통합 프로파일에서 얻은 측정값들의 편차 또는 산포를 나타냅니다.')
+            st.write('평균값으로부터 얼마나 떨어져 있는지를 평가하여 데이터의 퍼짐 정도를 표현합니다.')
+            st.write('표준 편차가 크면 천체의 펄스 강도가 불규칙적으로 변하는 것을 의미할 수 있습니다.')
+            st.markdown('- **Excess Kurtosis of the Integrated Profile (통합 프로파일의 첨도)**')
+            st.write('첨도는 확률 변수의 분포에서 꼬리 부분의 상대적인 두께를 나타내는 지표로서, 데이터가 정규 분포에서 얼마나 벗어난 분포를 갖고 있는지를 나타냅니다.')
+            st.write('첨도가 0보다 크면 뾰족한 분포로, 0보다 작으면 완만한 분포로 해석할 수 있습니다.')
+            st.markdown('- **Skewness of the Integrated Profile (통합 프로파일의 비대칭도)**')
+            st.write('데이터의 비대칭 정도를 나타내는 지표로서, 평균을 기준으로 얼마나 좌우로 치우쳐져 있는지를 나타냅니다.')
+            st.write('양수면 오른쪽으로, 음수면 왼쪽으로 치우쳐진 분포를 의미합니다.')
+            st.markdown('- **Mean of the DM-SNR Curve (DM-SNR 곡선의 평균)**')
+            st.write('DM-SNR 곡선은 주기성이 있는 신호에서 찾아지는 신호 대 잡음 비율(DM-SNR) 값을 나타내는 곡선입니다.')
+            st.write('이 곡선에서 추출한 측정값들의 평균을 나타냅니다.')
+            st.markdown('- **Standard Deviation of the DM-SNR Curve (DM-SNR 곡선의 표준 편차)**')
+            st.write('DM-SNR 곡선에서 추출한 측정값들의 편차 또는 산포를 나타냅니다.')
+            st.write('평균값으로부터 얼마나 떨어져 있는지를 평가하여 데이터의 퍼짐 정도를 표현합니다.')
+            st.markdown('- **Excess Kurtosis of the DM-SNR Curve (DM-SNR 곡선의 첨도)**')
+            st.write('DM-SNR 곡선에서 추출한 측정값들의 첨도를 나타냅니다.')
+            st.write('이 값이 0보다 크면 뾰족한 분포로, 0보다 작으면 완만한 분포로 해석할 수 있습니다.')
+            st.markdown('- **Skewness of the DM-SNR Curve (DM-SNR 곡선의 비대칭도)**')
+            st.write('DM-SNR 곡선에서 추출한 측정값들의 비대칭 정도를 나타냅니다.')
+            st.write('양수면 오른쪽으로, 음수면 왼쪽으로 치우쳐진 분포를 의미합니다.')
+            st.markdown('- **traget_class**')
+            st.write('1인 경우 중성자별, 0인 경우 중성자별 X')
     
+
+        elif selected_sub_menu == "데이터 프레임 보기":
+            st.header("중상자별 데이터")
+            st.dataframe(df_abal)
+
+        else:
+            st.write("중성자별 데이터 인사이트가 들어갈 페이지입니다")
+
 
     if selected_menu == "데이터 시각화":
         columns_list =  df_star.columns.to_list()
@@ -269,10 +296,17 @@ if choose == "강판(Steel)":
     binary_column_list = df_binary.columns.to_list()
     multi_column_list = df_multi.columns.to_list()
     graph_list = ['kdeplot', 'histplot', 'boxplot']
-    if selected_menu == "데이터 설명":
-            st.header("강판 결함")
-            st.image("steel.jpeg", use_column_width=True)
 
+    if selected_menu == "데이터 설명":
+        selected_sub_menu = option_menu(None, ["특성 설명", '이진분류 데이터 보기', "다중분류 데이터 보기"],
+                                        menu_icon="cast", default_index=0, orientation="horizontal", 
+                                        styles = {"container": {"padding": "0!important", "background-color": "#fafafa"},
+                                                    "nav-link": {"font-size": "15px", "text-align": "center", "margin":"10px", "--hover-color": "#FFE192"},
+                                                    "icon": {"color": "orange", "font-size": "18px"}, 
+                                                    "nav-link-selected": {"background-coloaddr": "#FFC939"},
+                                                })
+        
+        if selected_sub_menu == "특성 설명":
             st.write('X_Minimum: 결함이 있는 영역의 X 좌표 중 최소값 -> Area 만들고 제거')
             st.write('X_Maximum: 결함이 있는 영역의 X 좌표 중 최대값 -> Area 만들고 제거')
             st.write('Y_Minimum: 결함이 있는 영역의 Y 좌표 중 최소값 -> Area 만들고 제거')
@@ -306,9 +340,23 @@ if choose == "강판(Steel)":
             st.write('SigmoidOfAreas: 결함 영역의 픽셀 면적에 대한 시그모이드 값')
             st.write('Type: 강철판 결함의 종류 (다중 분류를 위한 목표 변수) -> 이것도 원핫인코딩 형태를 하나의 컬럼으로 정의하고 라벨인코딩 실시)')
 
+        elif selected_sub_menu == "이진분류 데이터 보기":
+            with st.sidebar:
+                binary_option = st.selectbox('확인하고 싶은 특성을 선택하세요', df_binary['Type'].unique())
+            st.header("강판 결함")
+            st.write('''0 -> weak_defect(약한 결함)
+                    1 -> strong_defect(심한 결함)
+                    ''')
+            st.dataframe(df_binary)
 
+        else:
+            with st.sidebar:
+                multi_option = st.selectbox('확인하고 싶은 특성을 선택하세요', df_multi['Type'].unique())
+            st.header("강판 결함")
+            st.dataframe(df_multi)
+    
     if selected_menu == '데이터 시각화':
-        
+
         st.sidebar.title('Steel Features')
         st.sidebar.write('<div style="font-size: 14px; color: black;">이중분류 데이터와 다중분류 데이터가 같이 표시됩니다.</div>', unsafe_allow_html=True)
         select_features = st.sidebar.selectbox(
@@ -321,7 +369,7 @@ if choose == "강판(Steel)":
             df_multi_kde = df_multi.copy()
             df_multi_kde['Type'] = encoder.fit_transform(df_multi_kde['Type'])
             # 서브플롯 생성
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+            fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 10))
             sns.kdeplot(data=df_binary, x=select_features, ax=ax1)
             ax1.set_title(f'Binary {select_features} Kernel')
             sns.kdeplot(data=df_multi_kde, x=select_features, ax=ax2)
@@ -333,7 +381,7 @@ if choose == "강판(Steel)":
             df_binary_hist = df_binary.copy()
             df_binary_hist['Type'] = df_binary_hist['Type'].apply(lambda x: 'weak_defect' if x == 0 else 'strong_defect')
             # 서브플롯 생성
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+            fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 10))
             sns.histplot(data=df_binary_hist, x=select_features, ax=ax1)
             ax1.set_title(f'Binary {select_features} Kernel')
             sns.histplot(data=df_multi, x=select_features, ax=ax2)
@@ -343,7 +391,9 @@ if choose == "강판(Steel)":
             if select_features == 'Type':
                 st.write(f'<div style="font-size: 20px; color: red;">{select_features}은 현재 문자열이므로 {select_graph}을 확인 할 수 없습니다.</div>', unsafe_allow_html=True)
             else:
-                fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+                fig = plt.figure(figsize=(15, 10))
+                ax1 = fig.add_subplot(2, 1, 1)
+                ax2 = fig.add_subplot(2, 1, 2)
                 sns.boxplot(data=df_binary, x=select_features, ax=ax1)
                 ax1.set_title(f'Binary {select_features} Kernel')
                 sns.boxplot(data=df_multi, x=select_features, ax=ax2)
@@ -353,5 +403,6 @@ if choose == "강판(Steel)":
 
     if selected_menu == '모델 예측':
         pass
+                
     else:
         pass
